99f0cfcc460ad731172a3a7c07ce01e5a11e82a1
paullsshin <paullsshin@hotmail.com>
2023-02-01 16:50:53 -0800

    trying to figure out why the ID is coming up as undefined

--- index.js
+++ index.js
@@ -11,71 +11,35 @@ class Group {
   constructor() {
     this.groupMembers = [];
 
-    this.memberRole = "Manager";
+    this.memberPosition = "Manager";
   }
 
-  askQuestions() {
-    if (this.memberRole === "Manager") {
+  memberInput() {
+    if (this.memberPosition === "Manager") {
       inquirer
         .prompt([
           {
             type: "input",
             name: "managersName",
             message: "What is the name of your manager?",
-            validate: (managersName) => {
-              if (managersName.length > 0) {
-                return true;
-              } else {
-                console.log("Enter a valid name for the manager");
-                return false;
-              }
-            },
           },
 
           {
             type: "input",
             name: "managersID",
             message: "What is the ID of your manager?",
-            validate: (managersId) => {
-              if (managersId.length > 0) {
-                return true;
-              } else {
-                console.log("Enter a valid ID for the manager");
-                return false;
-              }
-            },
           },
 
           {
             type: "input",
             name: "managersEmail",
             message: "What is the email address of your manager?",
-            validate: (managersEmail) => {
-              if (
-                /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(
-                  managersEmail
-                )
-              ) {
-                return true;
-              } else {
-                console.log("Enter a valid email address for the manager");
-                return false;
-              }
-            },
           },
 
           {
             type: "input",
             name: "managersOfficeNumber",
             message: "What is the office number of your manager?",
-            validate: (managersOfficeNumber) => {
-              if (managersOfficeNumber.length > 0) {
-                return true;
-              } else {
-                console.log("Enter a valid phone number for the manager");
-                return false;
-              }
-            },
           },
 
           {
@@ -87,7 +51,7 @@ class Group {
         ])
 
         .then((managerSelector) => {
-          this.memberRole = managerSelector.pickAMember;
+          this.memberPosition = managerSelector.pickAMember;
           const managerStats = new Manager(
             managerSelector.managersName,
             managerSelector.managersId,
@@ -95,69 +59,33 @@ class Group {
             managerSelector.managersOfficeNumber
           );
           this.groupMembers.push(managerStats);
-          this.askQuestions();
+          this.memberInput();
         });
-    } else if (this.memberRole === "Engineer") {
+    } else if (this.memberPosition === "Engineer") {
       inquirer
         .prompt([
           {
             type: "input",
             name: "engineersName",
             message: "What is the name of your Engineer?",
-            validate: (engineersName) => {
-              if (engineersName.length > 0) {
-                return true;
-              } else {
-                console.log("Enter a valid name for the engineer");
-                return false;
-              }
-            },
           },
 
           {
             type: "input",
             name: "engineersID",
             message: "What is the ID of your Engineer?",
-            validate: (engineersId) => {
-              if (parseInt(engineersId) > 0) {
-                return true;
-              } else {
-                console.log("Enter a valid ID for the Engineer");
-                return false;
-              }
-            },
           },
 
           {
             type: "input",
             name: "engineersEmail",
             message: "What is the email address of your engineer?",
-            validate: (engineersEmail) => {
-              if (
-                /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(
-                  engineersEmail
-                )
-              ) {
-                return true;
-              } else {
-                console.log("Enter a valid email address for the Engineer");
-                return false;
-              }
-            },
           },
 
           {
             type: "input",
             name: "engineersGithub",
             message: "What is the Github for your Engineer?",
-            validate: (engineersGithub) => {
-              if (engineersGithub.length > 0) {
-                return true;
-              } else {
-                console.log("Enter a valid Github for the Engineer");
-                return false;
-              }
-            },
           },
 
           {
@@ -169,7 +97,7 @@ class Group {
         ])
 
         .then((engineerSelector) => {
-          this.memberRole = engineerSelector.pickAMember;
+          this.memberPosition = engineerSelector.pickAMember;
           const engineerStats = new Engineer(
             engineerSelector.engineersName,
             engineerSelector.engineersId,
@@ -177,69 +105,33 @@ class Group {
             engineerSelector.engineersGithub
           );
           this.groupMembers.push(engineerStats);
-          this.askQuestions();
+          this.memberInput();
         });
-    } else if (this.memberRole === "Intern") {
+    } else if (this.memberPosition === "Intern") {
       inquirer
         .prompt([
           {
             type: "input",
             name: "internsName",
             message: "What is the name of your Intern?",
-            validate: (internsName) => {
-              if (internsName.length > 0) {
-                return true;
-              } else {
-                console.log("Enter a valid name for the Intern");
-                return false;
-              }
-            },
           },
 
           {
             type: "input",
             name: "internsID",
             message: "What is the ID of your Intern?",
-            validate: (internsId) => {
-              if (parseInt(internsId) > 0) {
-                return true;
-              } else {
-                console.log("Enter a valid ID for the Intern");
-                return false;
-              }
-            },
           },
 
           {
             type: "input",
             name: "internsEmail",
             message: "What is the email address of your Intern?",
-            validate: (internsEmail) => {
-              if (
-                /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(
-                  internsEmail
-                )
-              ) {
-                return true;
-              } else {
-                console.log("Enter a valid email address for the Intern");
-                return false;
-              }
-            },
           },
 
           {
             type: "input",
             name: "internsSchool",
             message: "What school does your Intern attend?",
-            validate: (internsSchool) => {
-              if (internsSchool.length > 0) {
-                return true;
-              } else {
-                console.log("Enter a valid school name for your Intern");
-                return false;
-              }
-            },
           },
 
           {
@@ -251,7 +143,7 @@ class Group {
         ])
 
         .then((internSelector) => {
-          this.memberRole = internSelector.pickAMember;
+          this.memberPosition = internSelector.pickAMember;
           const internStats = new Intern(
             internSelector.internsName,
             internSelector.internsId,
@@ -259,7 +151,7 @@ class Group {
             internSelector.internsSchool
           );
           this.groupMembers.push(internStats);
-          this.askQuestions();
+          this.memberInput();
         });
     } else {
       fs.writeFile("dist/index.html", createHtml(this.groupMembers), (error) =>
@@ -272,4 +164,4 @@ class Group {
 }
 
 const group = new Group();
-group.askQuestions();
+group.memberInput();
--- src/createHtml.js
+++ src/createHtml.js
@@ -1,3 +1,81 @@
 const createHtml = (employeeStats) => {
-    return ``
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+      </head>
+        <link rel="stylesheet" href="./style.css">
+        <title>I Need a Team!</title>
+    </head>
+    
+    <body>
+    <header id="top">MY ASSEMBLED TEAM</header>
+<div class="container">
+        ${createCard(employeeStats)}
+</div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+</body>
+    `;
+};
+
+const createCard = (employeeStats) => {
+    let cardInfo = '';
+    employeeStats.forEach((employee) => {
+        if (employee.getPosition() === 'Manager') {
+            cardInfo += 
+            ` 
+            <div class="card" style="width: 18rem;">
+            <div class="card-body">
+            <h5 class="card-title">${employee.getName()}</h5>
+            <p class="card-text"> 💼 ${employee.getPosition()}</p>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: ${employee.getId()}</li>
+            <li class="list-group-item">EMAIL: <a href="mailto:${employee.getEmail()}" class="card-link">${employee.getEmail()}</a></li>
+            <li class="list-group-item">OFFICE NUMBER: <a href="#" class="card-link">${employee.getOfficeNumber()}</a></li>
+          </ul>
+        </div>
+        `;
+        } else if (employee.getPosition() === 'Engineer') {
+            cardInfo += 
+            `
+            <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">${employee.getName()}</h5>
+          <p class="card-text">👷 ${employee.getPosition()}</p>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">ID: ${employee.getId()}</li>
+          <li class="list-group-item">EMAIL: <a href="mailto:${employee.getEmail()}" class="card-link">${employee.getEmail()}</a></li>
+          <li class="list-group-item">GITHUB: <a href="https://github.com/${employee.getGithub()}" class="card-link">${employee.getGithub()}</a></li>
+        </ul>
+      </div>
+        `;
+        } else if (employee.getPosition() === 'Intern') {
+            cardInfo +=
+            `
+            <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">${employee.getName()}</h5>
+          <p class="card-text">🧑‍🎓 ${employee.getPosition()}</p>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">ID: ${employee.getId()}</li>
+          <li class="list-group-item">EMAIL: <a href="mailto:${employee.getEmail()}" class="card-link">${employee.getEmail()}</a></li>
+          <li class="list-group-item">SCHOOL: <a href="https://www.southharmoninstituteoftechnology.org/" class="card-link">${employee.getSchool()}</a></li>
+        </ul>
+      </div>
+    </div>
+         `;
+        }
+    });
+
+    return cardInfo;
 }
+
+module.exports = createHtml;
